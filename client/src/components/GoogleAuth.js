2269830a6820697a92c2f45a42139884a99e1728
kamalogudah <mcpaul2058@gmail.com>
2019-12-26 00:11:18 +0300

    On demand Sign In and Sign Out

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,41 +1,49 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-
   state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: '412317181939-i82s9pf6k7ecdhpu590van5v0i55heq2.apps.googleusercontent.com',
-        scope: 'email'
-      }).then(() => {
-        this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-        this.auth.isSignedIn.listen(this.onAuthChange);
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '412317181939-i82s9pf6k7ecdhpu590van5v0i55heq2.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
 
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-  }
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+     this.auth.signOut();
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={ this.onSignOut } className="ui red google button">
           <i className="google icon" />
           Sign Out
-
         </button>
-
       );
     } else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignIn}  className="ui red google button">
           <i className="google icon" />
           Sign in with Google
         </button>
@@ -45,7 +53,6 @@ class GoogleAuth extends React.Component {
   render() {
     return <div> {this.renderAuthButton()} </div>;
   }
-
 }
 
 export default GoogleAuth;
