604adec418da2d329779f41aec49ec5a8b2d3869
kamalogudah <mcpaul2058@gmail.com>
2019-12-26 01:19:09 +0300

    Handling Auth Status through Redux

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -6,7 +6,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SIGN_IN':
       return { ...state, isSignedIn: true };
-    case 'SIGN_IN':
+     
+    case 'SIGN_OUT':
       return { ...state, isSignedIn: false };
     default:
       return state;
