97d5549a21312d080eddef7907f157cf45335a97
kamalogudah <mcpaul2058@gmail.com>
2019-12-26 01:04:02 +0300

    Building the auth Reader

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import authReducer from '../reducers/authReducer';
 
 export default combineReducers({
-  replaceMe: () => 'get'
+  auth: authReducer
 
 });
