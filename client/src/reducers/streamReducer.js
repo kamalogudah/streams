63f1c27b7ff75f407c114ba03d084cb09a58c470
kamalogudah <mcpaul2058@gmail.com>
2020-01-06 11:18:49 +0300

    Merging Lists of Records

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -3,6 +3,8 @@ import { FETCH_STREAM, CREATE_STREAM, FETCH_STREAMS, EDIT_STREAM, DELETE_STREAM 
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
       return { ...state, [action.payload.id] : action.payload };
     case CREATE_STREAM:
