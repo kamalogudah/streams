115393ac07ca5a5a0eca58125a16db1557f9124f
kamalogudah <mcpaul2058@gmail.com>
2020-05-29 17:05:32 +0300

    Selecting records from state

--- client/src/components/streams/StreamEdit.jsx
+++ client/src/components/streams/StreamEdit.jsx
@@ -5,4 +5,8 @@ const StreamEdit = (props) => {
   return <div> StreamEdit </div>;
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+
+}
+export default connect(mapStateToProps)(StreamEdit);
